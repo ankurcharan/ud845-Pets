05794c867df8b4e2006db156b6618e71c573c98f
Ankur Charan <ankurcharan98@gmail.com>
2020-07-03 15:58:16 +0530

    adding uri match
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,8 +1,13 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class PetContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
 
     private PetContract() {}
 
@@ -18,5 +23,7 @@ public class PetContract {
         public final static int GENDER_UNKNOWN = 0;
         public final static int GENDER_MALE = 1;
         public final static int GENDER_FEMALE = 2;
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
     }
 }
